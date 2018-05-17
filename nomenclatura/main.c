97eee6eee590a6d5d4caffbd654b2526dcf9aa34
lionelherrerobattista <lionherrero@gmail.com>
2018-05-17 10:23:14 -0300

    Punteros 1

--- nomenclatura/main.c
+++ nomenclatura/main.c
@@ -3,6 +3,32 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //punteros
+    int *punteroNumero; //puntero y nombre variable
+    int numero;
+    int numeroDos;
+    numero=66;
+    //&numeroDos=&numero;
+    punteroNumero=&numero;
+    numeroDos=&numero;//Le asigno la dirección de memoria
+    printf("\na-%d",numeroDos);
+    numeroDos=*punteroNumero;//Le asigno valor
+    printf("\nb-%d",numeroDos);
+
+    //*punteroNumero=55; No es un entero, es un puntero a entero.
+    //                   No le puedo asignar un valor
+    //Le asigno direccion de memoria:
+    //*punteroNumero=&numero; Da error
+    punteroNumero=&numero;
+    printf("\n%d",numero);
+    printf("\n%d",numeroDos);
+    //Misma dirección de memoria
+    printf("\n%p",&numero);
+    printf("\nc-%p",punteroNumero); //Dirección de la variable numero
+    printf("\nd-%p",&punteroNumero);//Dirección donde está guardado el puntero.
+    //Necesito saber donde empieza (dirección)
+    //printf("%d",*punteroNumero); error
+    printf("\ne-%d",*punteroNumero);//Muestra lo que está en el puntero.
+    //                              Hay que inicializarlo con algun valor
     return 0;
 }
